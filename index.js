35850aec01064e98a489fdab0fb14f33ede3c94f
nagaops <nagasatish54@gmail.com>
2022-10-14 16:34:16 +0000

    2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = ' Aws and Devops-Sessions-1 ---- beanstalk
+  const msg = ' Aws and Devops-Sessions-1 ---- beanstalk'
   res.end(msg);
 });
 
